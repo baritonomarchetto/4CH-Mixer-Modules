b4763aa14f9ee23e6345f94f2dd071cf43a03a31
baritonomarchetto <baritonomarchetto77@gmail.com>
2025-01-15 13:46:18 +0100

    Add files via upload

---
+++ VERSION_2/A440_toneGen.ino
@@ -0,0 +1,27 @@
+/* Reference Tone generator
+ * Part of 4-Channel mixer project (synthbrigade.altervista.org)
+ * 
+ * by Barito
+ */
+
+const byte corrPin = 3;
+const byte tonePin = 4;
+
+int pitchRaw;
+int pitchCorr;
+int baseFreq = 440; // A4
+
+void setup() {
+  pinMode(corrPin, INPUT);
+  pinMode(tonePin, OUTPUT);
+  tone(tonePin, baseFreq); //A4
+}
+
+void loop() {
+  if (analogRead(refPin) ! = pitchRaw){
+    pitchRaw = analogRead(refPin); // 0/1023
+    pitchCorr = pitchRaw >> 5; // 0/31
+    pitchCorr = pitchCorr - pitchCorr/2; // -15/+15
+    tone(tonePin, baseFreq + pitchCorr);
+  }
+}
