d0ad53c38e88e6929459c1ccfa97fdc9e421958a
baritonomarchetto <baritonomarchetto77@gmail.com>
2025-01-16 13:20:07 +0100

    Update A440_toneGen.ino

--- VERSION_2/A440_toneGen.ino
+++ VERSION_2/A440_toneGen.ino
@@ -7,21 +7,16 @@
 const byte corrPin = 3;
 const byte tonePin = 4;
 
-int pitchRaw;
 int pitchCorr;
 int baseFreq = 440; // A4
 
 void setup() {
   pinMode(corrPin, INPUT);
   pinMode(tonePin, OUTPUT);
-  tone(tonePin, baseFreq); //A4
+  //tone(tonePin, baseFreq); //A4
 }
 
 void loop() {
-  if (analogRead(corrPin) != pitchRaw){
-    pitchRaw = analogRead(corrPin); // 0/1023
-    pitchCorr = pitchRaw >> 5; // 0/31
-    pitchCorr = pitchCorr - pitchCorr/2; // -15/+15
-    tone(tonePin, baseFreq + pitchCorr);
-  }
+  pitchCorr = analogRead(corrPin) >> 7; // 0/7
+  tone(tonePin, baseFreq + pitchCorr*2 -7);
 }
